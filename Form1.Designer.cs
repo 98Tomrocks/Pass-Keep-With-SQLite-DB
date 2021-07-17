59c9172189c9e0fdc8939aa5d9c463f0c4ff970f
Thomas Nichols <rockstommy98@gmail.com>
2021-07-17 17:49:16 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace CSharp_Final_Project__Thomas_Nichols
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lstPossible = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstAnswer = new System.Windows.Forms.ListBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNewPass = new System.Windows.Forms.TextBox();
+            this.txtNewUser = new System.Windows.Forms.TextBox();
+            this.txtNewProg = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name of Program :";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(106, 21);
+            this.txtName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(160, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // lstPossible
+            // 
+            this.lstPossible.FormattingEnabled = true;
+            this.lstPossible.Location = new System.Drawing.Point(11, 71);
+            this.lstPossible.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lstPossible.Name = "lstPossible";
+            this.lstPossible.Size = new System.Drawing.Size(93, 264);
+            this.lstPossible.TabIndex = 5;
+            this.lstPossible.SelectedIndexChanged += new System.EventHandler(this.lstPossible_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 57);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Posssible Programs";
+            // 
+            // lstAnswer
+            // 
+            this.lstAnswer.FormattingEnabled = true;
+            this.lstAnswer.Location = new System.Drawing.Point(106, 86);
+            this.lstAnswer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lstAnswer.Name = "lstAnswer";
+            this.lstAnswer.Size = new System.Drawing.Size(160, 30);
+            this.lstAnswer.TabIndex = 6;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(106, 42);
+            this.btnFind.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(159, 23);
+            this.btnFind.TabIndex = 2;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(4, 99);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(69, 29);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(77, 98);
+            this.btnChange.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(77, 29);
+            this.btnChange.TabIndex = 4;
+            this.btnChange.Text = "Change";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(4, 131);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(69, 29);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(106, 305);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(159, 29);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtNewPass);
+            this.groupBox1.Controls.Add(this.txtNewUser);
+            this.groupBox1.Controls.Add(this.txtNewProg);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.btnChange);
+            this.groupBox1.Controls.Add(this.btnDelete);
+            this.groupBox1.Location = new System.Drawing.Point(106, 119);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(159, 164);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Modifying Section";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(77, 131);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 28);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear(All)";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(4, 71);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 52);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 31);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Program";
+            // 
+            // txtNewPass
+            // 
+            this.txtNewPass.Location = new System.Drawing.Point(64, 69);
+            this.txtNewPass.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNewPass.Name = "txtNewPass";
+            this.txtNewPass.Size = new System.Drawing.Size(91, 20);
+            this.txtNewPass.TabIndex = 2;
+            // 
+            // txtNewUser
+            // 
+            this.txtNewUser.Location = new System.Drawing.Point(64, 48);
+            this.txtNewUser.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNewUser.Name = "txtNewUser";
+            this.txtNewUser.Size = new System.Drawing.Size(91, 20);
+            this.txtNewUser.TabIndex = 1;
+            // 
+            // txtNewProg
+            // 
+            this.txtNewProg.Location = new System.Drawing.Point(64, 27);
+            this.txtNewProg.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNewProg.Name = "txtNewProg";
+            this.txtNewProg.Size = new System.Drawing.Size(91, 20);
+            this.txtNewProg.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(107, 71);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Response:";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnFind;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 345);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.lstAnswer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstPossible);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Password Keeper";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.ListBox lstPossible;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstAnswer;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNewPass;
+        private System.Windows.Forms.TextBox txtNewUser;
+        private System.Windows.Forms.TextBox txtNewProg;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Data.Sqlite;
+using System.Security.Cryptography;
+
+namespace CSharp_Final_Project__Thomas_Nichols
+{
+    
+
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+ 
+            toolTip1.SetToolTip(groupBox1,"You can add Info by pressing the add button. It will not work if it isn't a new program.\n" +
+                "To use the change key enter an already exsisting program name, but change the user name or password.\n" +
+                "To delete, simply put the program name in the corresponding box above.\n" +
+                "Changes will take place AFTER reset of program.");//Tool tip to explain adding, changing, and deleting
+
+            var con = new SqliteConnection(@"Data Source = AllInOneDb.db;");
+            con.Open();
+            string stm = "SELECT ProgramName FROM Info;";
+            var UserReadTest = new SqliteCommand(stm, con);
+            SqliteDataReader Read = UserReadTest.ExecuteReader();
+            while(Read.Read())
+            {
+                lstPossible.Items.Add(Encoding.ASCII.GetString(Convert.FromBase64String(Read.GetString(0))));
+            }
+            con.Close();
+        }
+        private void lstPossible_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            if(lstPossible.SelectedItem!=null)
+            {
+                txtName.Text = lstPossible.SelectedItem.ToString();
+                txtNewProg.Text = lstPossible.SelectedItem.ToString();
+                lstAnswer.Items.Clear();
+                var con = new SqliteConnection(@"Data Source = AllInOneDb.db;");
+                con.Open();
+                string stm = "SELECT * FROM Info WHERE ProgramName=$Program";
+                var UserReadTest = new SqliteCommand(stm, con);
+                UserReadTest.Parameters.AddWithValue("$Program", Convert.ToBase64String(Encoding.ASCII.GetBytes(txtName.Text)));
+                SqliteDataReader Read = UserReadTest.ExecuteReader();
+
+                while (Read.Read())
+                {
+
+                    lstAnswer.Items.Add(Encoding.ASCII.GetString(Convert.FromBase64String(Read.GetString(1))));
+                    lstAnswer.Items.Add(Encoding.ASCII.GetString(Convert.FromBase64String(Read.GetString(2))));
+                }
+                con.Close();
+            }
+            
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            //Displays the username and password for supplied program name
+            lstAnswer.Items.Clear();
+            var con = new SqliteConnection(@"Data Source = AllInOneDb.db;");
+            con.Open();
+            string stm = "SELECT * FROM Info WHERE ProgramName=$Program";
+            var UserReadTest = new SqliteCommand(stm, con);
+            UserReadTest.Parameters.AddWithValue("$Program", Convert.ToBase64String(Encoding.ASCII.GetBytes(txtName.Text)));
+            SqliteDataReader Read = UserReadTest.ExecuteReader();
+
+            while (Read.Read())
+            {
+
+                lstAnswer.Items.Add(Encoding.ASCII.GetString(Convert.FromBase64String(Read.GetString(1))));
+                lstAnswer.Items.Add(Encoding.ASCII.GetString(Convert.FromBase64String(Read.GetString(2))));
+            }
+            con.Close();
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            //Adds an entry to the files
+            lstAnswer.Items.Clear();
+            var con = new SqliteConnection(@"Data Source = AllInOneDb.db;");
+            con.Open();
+            string stm = "SELECT * FROM Info WHERE ProgramName=$Program";
+            var UserReadTest = new SqliteCommand(stm, con);
+            UserReadTest.Parameters.AddWithValue("$Program", Convert.ToBase64String(Encoding.ASCII.GetBytes(txtNewProg.Text)));
+            SqliteDataReader Read = UserReadTest.ExecuteReader();
+            var response = Read.Read();
+            if(response==false)
+            {
+
+                var cmd = new SqliteCommand("INSERT INTO Info(ProgramName, Username, Password) VALUES($NewProgram,$User_Name,$Pass_Word)", con);
+                cmd.Parameters.AddWithValue("$NewProgram", Convert.ToBase64String(Encoding.ASCII.GetBytes(txtNewProg.Text)));
+                cmd.Parameters.AddWithValue("$User_Name", Convert.ToBase64String(Encoding.ASCII.GetBytes(txtNewUser.Text)));
+                cmd.Parameters.AddWithValue("$Pass_Word", Convert.ToBase64String(Encoding.ASCII.GetBytes(txtNewPass.Text)));
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+                RefreshList();
+
+                txtNewProg.Text = "";
+                txtNewUser.Text = "";
+                txtNewPass.Text = "";
+            }
+            con.Close();
+            lstAnswer.Items.Add("Program Already Exists");
+            
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            //Changes a preexsisting file
+            lstAnswer.Items.Clear();
+
+            var con = new SqliteConnection(@"Data Source = AllInOneDb.db;");
+            var cmd = new SqliteCommand("DELETE FROM Info WHERE ProgramName=$Program", con);
+            cmd.Parameters.AddWithValue("$Program", Convert.ToBase64String(Encoding.ASCII.GetBytes(txtNewProg.Text)));
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            
+            cmd = new SqliteCommand("INSERT INTO Info(ProgramName, Username, Password) VALUES($NewProgram,$User_Name,$Pass_Word)", con);
+            cmd.Parameters.AddWithValue("$NewProgram", Convert.ToBase64String(Encoding.ASCII.GetBytes(txtNewProg.Text)));
+            cmd.Parameters.AddWithValue("$User_Name", Convert.ToBase64String(Encoding.ASCII.GetBytes(txtNewUser.Text)));
+            cmd.Parameters.AddWithValue("$Pass_Word", Convert.ToBase64String(Encoding.ASCII.GetBytes(txtNewPass.Text)));
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            RefreshList();
+            lstAnswer.Items.Clear();
+            txtNewProg.Text = "";
+            txtNewUser.Text = "";
+            txtNewPass.Text = "";
+            
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            //Deletes an entry
+            lstAnswer.Items.Clear();
+            var con = new SqliteConnection(@"Data Source = AllInOneDb.db;");
+            var cmd = new SqliteCommand("DELETE FROM Info WHERE ProgramName=$Program", con);
+            cmd.Parameters.AddWithValue("$Program", Convert.ToBase64String(Encoding.ASCII.GetBytes(txtNewProg.Text)));
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            RefreshList();
+            lstAnswer.Items.Clear();//Clears some text boxes and list box that displays responses
+            txtNewProg.Text = "";
+            txtNewUser.Text = "";
+            txtNewPass.Text = "";
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();//Exit button
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstAnswer.Items.Clear();//Clears some text boxes and list box that displays responses
+            txtNewProg.Text = "";
+            txtNewUser.Text = "";
+            txtNewPass.Text = "";
+        }
+
+        private void RefreshList()
+        {
+            lstPossible.Items.Clear();
+            var con = new SqliteConnection(@"Data Source = AllInOneDb.db;");
+            con.Open();
+            string stm = "SELECT ProgramName FROM Info;";
+            var UserReadTest = new SqliteCommand(stm, con);
+            SqliteDataReader Read = UserReadTest.ExecuteReader();
+            while (Read.Read())
+            {
+                lstPossible.Items.Add(Encoding.ASCII.GetString(Convert.FromBase64String(Read.GetString(0))));
+            }
+            con.Close();
+        }
+    }
+}
